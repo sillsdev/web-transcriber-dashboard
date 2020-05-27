cd71e90bd1a5f8ba5b3ce5234df02241ce78ae8a
Greg Trihus <greg_trihus@sil.org>
2020-05-27 17:01:21 -0500

    Remove unused static data

--- src/Report.tsx
+++ src/Report.tsx
@@ -38,25 +38,6 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
-function createData(
-  name: string,
-  total: number,
-  lastMonth: number,
-  lastWeek: number
-) {
-  return { name, total, lastMonth, lastWeek };
-}
-
-const rows = [
-  createData('Projects', 159, 20, 10),
-  createData('Training Projects', 15, 5, 1),
-  createData('Scripture Projects', 140, 25, 20),
-  createData('Plans', 305, 103, 55),
-  createData('Passages', 1300, 500, 100),
-  createData('Transcriptions', 1000, 400, 90),
-  createData('Paratext Sync', 403, 130, 45),
-];
-
 const StyledTableCell = withStyles((theme: Theme) =>
   createStyles({
     head: {
